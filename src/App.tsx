13d11dfac9638d7dca32e51e17c26de14b066632
datodia <dato.diasamidze.02@gmail.com>
2024-02-07 22:33:24 +0400

    Init: Initialize Project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='w-[343px]'>
+
+      <h1 className='text-[var(--gray)] font-bold text-center text-base '>Password Generator</h1>
+    </div>
+  )
+}
+
+export default App
